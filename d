f576b0f6b6e309a789251a1531ff41019d8bb2d7
Jardeson <jardeson777@gmail.com>
2021-02-20 17:21:59 -0200

    Mudei o backgroud e coloquei a condição para caso a pessoa perca

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit 037848726ca37c2465f05de1cabcec0097484a91[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Jardeson <jardeson777@gmail.com>
+Date:   Sat Feb 20 14:51:46 2021 -0200
+
+    Agora os botões de vivo e morto funcionam
+
+[33mcommit 9cdef9d131ba291f2b0b0c2a65a689eef5c114e3[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Jardeson <jardeson777@gmail.com>
+Date:   Wed Feb 17 23:54:26 2021 -0200
+
+    Mudei de setTimeout para SetInterval e assim coloquei um metodo de parada do loop
+
+[33mcommit 21e079dbf5a5f2918183aec4d19b24d2e778c067[m
+Author: Jardeson <jardeson777@gmail.com>
+Date:   Wed Feb 17 22:09:19 2021 -0200
+
+    Modifiquei o modo com que as img são trocadas
+
+[33mcommit 3d22a9ac0daa0eaebaa6cf4b137b79ff972b0fbd[m
+Author: Jardeson <jardeson777@gmail.com>
+Date:   Mon Feb 15 15:43:13 2021 -0200
+
+    1
--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var indice = 0; //Para saber qual imagem que vai ser usada
 var cont = 0;
 var resposta = 2;
 var iniciaLoop;
+var iniciaLoop2;
 var pararLoop;
+var score = -1;
 var todasImagens = ["./image/abaixado2.png", "./image/levantado2.jpg"];
 
 function trocaImagem() {
@@ -13,18 +15,31 @@ function trocaImagem() {
     } else {
         indice = 0;
     }
-    cont = Math.floor(Math.random()*8);
+    document.getElementById("vivo").disabled = true;
+    document.getElementById("morto").disabled = true;
+    cont = Math.floor(Math.random()*8); //Gera um valor aleatorio, se esse valor for maior que 3 as imagens vão parar de ficar alternando
 }
 
 function iniciar() {
     if(resposta == indice | resposta == 2) {
+        score++;
+        document.getElementById("score").innerHTML = "score: " + score;
         iniciaLoop = setInterval(trocaImagem, 113); //Onde acontece a troca de imagem a cada tempo
+        document.getElementById("msgErro").innerHTML = "";
+    } else {
+        document.getElementById("msgErro").innerHTML = "Você perdeu!</br>Sua pontuação foi: " + score;
+        score = 0;
+        document.getElementById("score").innerHTML = "score: " + score;
     }
 }
 
 function paraLoop() {
     if(cont > 3) {
         clearInterval(iniciaLoop);
+        document.getElementById("vivo").disabled = false;
+        document.getElementById("morto").disabled = false;
+        resposta = 3;
+        iniciaLoop2 = setTimeout("iniciar()", 1000);
         cont = 0;
     }
     pararLoop = setTimeout("paraLoop()", 400);
@@ -34,6 +49,7 @@ function respostaVivo() {
     resposta = 0;
     console.log(indice);
     console.log(resposta);
+    clearTimeout(iniciaLoop2);
     iniciar();
 }
 
@@ -41,6 +57,7 @@ function respostaMorto() {
     resposta = 1;
     console.log(indice);
     console.log(resposta);
+    clearTimeout(iniciaLoop2);
     iniciar();
 }
 
