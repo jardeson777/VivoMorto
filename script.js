037848726ca37c2465f05de1cabcec0097484a91
Jardeson <jardeson777@gmail.com>
2021-02-20 14:51:46 -0200

    Agora os botões de vivo e morto funcionam

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var indice = 0; //Para saber qual imagem que vai ser usada
 var cont = 0;
+var resposta = 2;
+var iniciaLoop;
+var pararLoop;
 var todasImagens = ["./image/abaixado2.png", "./image/levantado2.jpg"];
 
 function trocaImagem() {
@@ -10,18 +13,42 @@ function trocaImagem() {
     } else {
         indice = 0;
     }
-    cont++;
-    console.log(cont);
-}
-
-function paraLoop() {
-    if(cont >=7) {
-        clearInterval(loop);
-        cont = 0;
-    }
-    setTimeout("paraLoop()", 400);
+    cont = Math.floor(Math.random()*8);
 }
 
 function iniciar() {
-    loop = setInterval(trocaImagem, 100); //Onde acontece a troca de imagem a cada tempo
+    if(resposta == indice | resposta == 2) {
+        iniciaLoop = setInterval(trocaImagem, 113); //Onde acontece a troca de imagem a cada tempo
+    }
+}
+
+function paraLoop() {
+    if(cont > 3) {
+        clearInterval(iniciaLoop);
+        cont = 0;
+    }
+    pararLoop = setTimeout("paraLoop()", 400);
+}
+
+function respostaVivo() {
+    resposta = 0;
+    console.log(indice);
+    console.log(resposta);
+    iniciar();
+}
+
+function respostaMorto() {
+    resposta = 1;
+    console.log(indice);
+    console.log(resposta);
+    iniciar();
+}
+
+function analisarResposta() {
+    if(resposta == indice) {
+        setTimeout("iniciar()", 1100);
+    } else {
+        clearInterval(iniciaLoop);
+        clearTimeout(paraLoop);
+    }
 }
